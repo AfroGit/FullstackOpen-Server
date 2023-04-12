f60f5562b00743d7922c9c5add9f9806fac03d37
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-04-12 14:35:51 +0000

    Install express in the terminal to help with server manipulation.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 Implement a Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons. */ 
 
-
+const express = require('express')
+const app = express()
 
 [
     { 
