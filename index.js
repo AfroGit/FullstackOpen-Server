4476b5b319b162a380d2a066eba20799bc62c1b5
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-04-18 02:15:26 +0000

    installed morgan as a parser

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({error: 'Name is Missing'})
   }
   
-  if(!body.number){//
+  if(!body.number){//confirms if entry is a digit
     return res.status(418).json({error: 'Number is Missing'})
   }
   
