0ef9de77cd3009206a2b141bec543cb19361f464
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-04-18 01:21:07 +0000

    Added a function that generates an ID on Ln 73
    Ln 73 function goes through the whole array and using map( ) creates a generic ID for any new entry made on the server.

--- index.js
+++ index.js
@@ -40,3 +40,72 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const currentDate = new Date()
+    res.send(`<h3>Phonebook has info for ${persons.length} people</h3><h4>${currentDate}</h4>`)
+})
+
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const entry = persons.find(entry => entry.id == id)
+    if(entry){
+        res.json(entry)
+        
+    } else{
+        res.status(404).end()
+    }
+})
+
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(entry => entry.id !=id)
+    res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  
+  if(!body.name){
+    return res.status(400).json({error: 'Name is Missing'})
+  }
+  
+  if(!body.number){
+    return res.status(418).json({error: 'Number is Missing'})
+  }
+  
+  if(persons.some(entry => entry.name === body.name)){
+    return res.status(409).json({error: 'Name Must Be Unique'})
+  }
+  
+  let entry = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  
+  persons.push(entry)//pushes entry, making it visible in object
+  res.json(entry)
+  
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server Active on ${PORT}`)
+})
